552367ea04fb537d38d3c2ad892d5d88b77148c6
Yash <ypt7895@gmail.com>
2020-12-02 13:26:41 +0530

    Added more routes and deleted unnecessary files

--- index.js
+++ index.js
@@ -31,6 +31,51 @@ app.post('/api/users/add',(req,res)=>{
             results:result
         });
     })
-})
+});
+
+app.get('/api/todos/:user',(req,res)=>{
+    let que = 'SELECT TodoId,Title,Status FROM Todos WHERE UserId=\"'+req.params.user+'\"';
+    con.query(que,(err,result,fields)=>{
+        if(err) throw err;
+        res.json({
+            success: true,
+            results: result
+        });
+    });
+});
+
+app.post('/api/users/check',(req,res)=>{
+    let data = {
+        UserId: req.body.UserId,
+        Password: req.body.Password
+    };
+    let que = 'SELECT UserId, Password FROM Users WHERE ?';
+    con.query(que,data,(err,result,fields)=>{
+        if(err) throw err;
+        res.json({
+            success: true,
+            results:result
+        });
+    })
+});
+
+app.post('/api/todos/add/:user',(req,res)=>{
+    let data = {
+        UserId: req.params.user,
+        //todo
+    };
+    let que = 'INSERT INTO Users SET ?';
+    con.query(que,data,(err,result,fields)=>{
+        if(err) throw err;
+        res.json({
+            success: true,
+            results:result
+        });
+    })
+});
+
+app.delete('/api/todos/delete',(req,res)=>{
+    //todo
+});
 
 app.listen(5000,()=>console.log("Started on 5000"));
--- routes/router.js
+++
@@ -1,9 +0,0 @@
-/*const express = require('express');
-const control = require('../controllers/controller');
-const router = express.Router();
-
-router.route('/users').get(control.getUsers);
-router.route('/tasks').get(control.getTasks);
-router.route('/users/add').post(control.addUser);
-
-module.exports = router;*/
