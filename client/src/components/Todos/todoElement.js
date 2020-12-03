71faa31dead726995d238e0d090f02059a6ab264
Yash <ypt7895@gmail.com>
2020-12-03 21:27:17 +0530

    basic list element

--- client/src/components/Todos/todoElement.js
+++ client/src/components/Todos/todoElement.js
@@ -2,21 +2,8 @@ import React from 'react';
 
 export default function Todo(props) {
     return (
-        <li className="todo stack-small" id={props.todoId}>
-          <div className="c-cb">
-            <input id={props.todoId} type="checkbox" defaultChecked={props.status===1} />
-            <label className="todo-label" htmlFor={props.todoId}>
-              {props.name}
-            </label>
-          </div>
-          <div className="btn-group">
-            <button type="button" className="btn">
-              Edit <span className="visually-hidden">{props.title}</span>
-            </button>
-            <button type="button" className="btn btn__danger">
-              Delete <span className="visually-hidden">{props.title}</span>
-            </button>
-          </div>
+        <li key={props.todoId}>
+          {props.title} <i>Completed: {props.status===0?"False":"True"}</i>
         </li>
       );
 }
